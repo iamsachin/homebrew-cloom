d2c54b29aa8a4fbf53e9fa953d913e30453ca749
Sachin Rajput <sachin.rajput.k@gmail.com>
2026-03-28 15:06:41 +0530

    Update cloom to 0.3.0

--- Casks/cloom.rb
+++ Casks/cloom.rb
@@ -1,8 +1,8 @@
 cask "cloom" do
-  version "0.2.0"
-  sha256 "8b87fe1fd0bc709beacee1786e187bd43535335fca675fe642684bf66ef1a525"
+  version "0.3.0"
+  sha256 "17b63d0e3e8ac2a7ab46181fa090b1bace2d7e8948a43a1ccdfef907f7afe462"
 
-  url "https://github.com/iamsachin/cloom/releases/download/v0.2.0/Cloom-0.2.0.dmg"
+  url "https://github.com/iamsachin/cloom/releases/download/v0.3.0/Cloom-0.3.0.dmg"
   name "Cloom"
   desc "Open-source screen recorder for macOS"
   homepage "https://github.com/iamsachin/cloom"
